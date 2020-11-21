27de567abcb1691697c878351e1f1e3c62fc8101
Sina-Sche <si.scheckenbach@gmail.com>
2020-11-21 15:54:57 +0100

    Fix babel loader version

--- server.js
+++ server.js
@@ -4,10 +4,6 @@ const path = require("path");
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get("/", (request, response) => {
-  response.send("Hello World!");
-});
-
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
 });
